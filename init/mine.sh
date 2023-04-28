8e5449bb885401a64f46e9d695f025747335c9b9
赵刘 <liulianjushi@noreply.example.org>
2023-04-28 10:21:11 +0800

    更新 'init/mine.sh'

--- init/mine.sh
+++ init/mine.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cp -r /workspace/keystore/* ~/data/keystore/
-geth --datadir ~/data/ --networkid 88 --http --http.addr 0.0.0.0 --http.port 7545 --port 30303 --http.api "admin,eth,miner,web3,personal,net,txpool,debug" --http.corsdomain "*" --unlock 0x23E53c3882B64B0907C831cFe7198c0a9a125e33 --password /workspace/password.txt  --allow-insecure-unlock  --nodiscover console
+geth --datadir ~/data/ --networkid 88 --http --http.addr 0.0.0.0 --http.port 7545 --port 30303 --http.api "admin,eth,miner,web3,personal,net,txpool,debug" --http.corsdomain "*" --mine --etherbase --unlock 0x23E53c3882B64B0907C831cFe7198c0a9a125e33 --password /workspace/password.txt  --allow-insecure-unlock  --nodiscover console
 
